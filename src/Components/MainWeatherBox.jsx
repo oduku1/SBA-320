58186ab5f10e37942e0c7acc5d99b6948819a656
Omar Dukureh <132103818+oduku1@users.noreply.github.com>
2025-11-13 21:56:43 -0500

    added data

--- src/Components/MainWeatherBox.jsx
+++ src/Components/MainWeatherBox.jsx
@@ -5,10 +5,30 @@ import { AuthContext } from "../context/Authcontext"
 
 
 export default function MainWeatherBox(){
-    const {currentWeather} = useContext(AuthContext)
+    const {currentWeather,location} = useContext(AuthContext)
+    const today = new Date()
+    const year = today.getFullYear();
+    const month = today.getMonth()+1;
+    const date = today.getDate()
+    const day  = today.getDay()
+
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ];
+      
     return(
         <div>
-                <button onClick={()=>{console.log(currentWeather)}}>Click</button>
+                <button onClick={()=>{console.log(currentWeather)}}>log weather</button>
+                <button onClick={()=>{console.log(today)}}>log date</button>
+
+                <div className="main-left">
+                <h3>{days[day]}</h3>
+                <h2>{month} {date}, {year}</h2>
+
+                </div>
+                
 
         
         </div>
